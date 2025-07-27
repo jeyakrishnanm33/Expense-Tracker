514d93e47249e699e55e67e4f04b6b693d3d1258
jeyakrishnanm33 <jaikrissh04@gmail.com>
2025-07-27 14:56:11 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const connectdb = async () => {
     try {
-        await mongoose.connect('mongodb+srv://jaikrissh04:Kannan%40312@cluster0.q7wmh2j.mongodb.net/test1');
         console.log('Expense database connected');
     } catch (error) {
         console.error('MongoDB connection failed:', error);
